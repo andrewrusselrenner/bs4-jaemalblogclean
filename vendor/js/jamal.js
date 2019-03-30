0b64c4c3ff01a4fc5d5b54822bad4d062574a884
andrewrusselrenner <talk.windows.inblog@live.com>
2019-03-30 16:38:42 +0800

    Update some changes as the new version will be updated

--- vendor/js/jamal.js
+++ vendor/js/jamal.js
@@ -1,8 +1,25 @@
-function load_home() {
-    document.getElementById("isi").innerHTML='<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="/hal/main.html" noscrolling></iframe></div>';
-}
+// function load_home() {
+    // document.getElementById("isi").innerHTML='test test';
+// }
 
 $( '#sidebar .navbar-nav a' ).on( 'click', function () {
     $( '#sidebar .navbar-nav' ).find( 'li.active' ).removeClass( 'show in active' );
     $( this ).parent( 'li' ).addClass( 'show in active' );
 });
+
+function load_rumah() {
+    $("#isi").load("/hal/main.html");
+}
+
+function load_tentang() {
+    $("#isi").load("/hal/tentang/index.html");
+}
+
+function load_bukutamu() {
+    $("#isi").load("/hal/guestbook/index.html");
+}
+
+function load_portofolio() {
+    $("#isi").load("/hal/portofolio/index.html");
+}
+
